91d5e0abc238453765382b11098b2f1f74fc73ba
vip <20frag08@mail.ru>
2022-08-21 19:28:30 +0300

    trial animation

--- lib/screens/animation_screen.dart
+++ lib/screens/animation_screen.dart
@@ -1,8 +1,49 @@
 import 'package:flutter/material.dart';
+// import 'animation_screen2.dart';
 
-import 'animation_screen2.dart';
+class AnimationScreen extends StatefulWidget {
 
-class AnimationScreen extends StatelessWidget {
+
+  @override
+  State<AnimationScreen> createState() => _AnimationScreenState();
+}
+
+class _AnimationScreenState extends State<AnimationScreen>
+    with SingleTickerProviderStateMixin {
+  //
+  // bool isAnimated = false;
+  //
+  // double width = 100;
+  // double height = 100;
+
+  late AnimationController _controller;
+
+  late Animation<double> animation;
+
+  final Tween<double> opacityTween = Tween<double>(begin: 0.0, end: 1.0);
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 400),
+    )
+      ..addListener(() {
+        setState(() {
+
+        });
+      });
+
+    animation = opacityTween.animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,18 +53,37 @@ class AnimationScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Hero(
-              tag: "My Hero",
-              child: Container(
-                width: 200,
-                height: 200,
-                color: Colors.yellow,
-              ),
+            Container(
+              width: 300,
+              height: 300,
+              color: Colors.yellow.withOpacity(animation.value),
+
+              // duration: Duration(milliseconds: 400),
             ),
             SizedBox(height: 10),
-            TextButton(onPressed: (){
-              Navigator.of(context).push(MaterialPageRoute(builder: (context)=>AnimationScreen2()));
-            }, child: Text("Go to screen 2"))
+            TextButton(onPressed: () {
+
+              if(_controller.isCompleted){
+                _controller.reverse();
+              } else {
+                _controller.forward();
+              }
+
+              // if(!isAnimated){
+              //  setState(() {
+              //    width = 300;
+              //    height = 300;
+              //  });
+              // } else {
+              //   setState(() {
+              //     width = 100;
+              //     height = 100;
+              //   });
+              // }
+              // isAnimated = !isAnimated;
+              // Navigator.of(context).push(MaterialPageRoute(builder: (context)=>AnimationScreen2()));
+            },
+                child: Text("animate"))
           ],
         ),
       ),
--- lib/screens/animation_screen2.dart
+++ lib/screens/animation_screen2.dart
@@ -1,27 +1,27 @@
-import 'package:flutter/material.dart';
-
-class AnimationScreen2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Hero(
-              tag: "My Hero",
-              child: Container(
-                width: 500,
-                height: 500,
-                color: Colors.blue,
-              ),
-            ),
-
-          ],
-        ),
-      ),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+//
+// class AnimationScreen2 extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           children: [
+//             Hero(
+//               tag: "My Hero",
+//               child: Container(
+//                 width: 500,
+//                 height: 500,
+//                 color: Colors.blue,
+//               ),
+//             ),
+//
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
